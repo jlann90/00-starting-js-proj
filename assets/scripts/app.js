755fa0c119b360f5af889dcc4e31b06a4600a506
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-07-18 13:57:46 -0700

    feat: add variable userMessage and note with difference between let and const

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,3 +2,7 @@
 // below is an example of import from file that has a an export of a default (single) item
 import apiKey from "./util.js";
 console.log(apiKey);
+
+// using 'let' to declare a variable allows for reassignment later, while 'const' is used for variables that should not change
+let userMessage = "Welcome to the JavaScript refresher course!";
+console.log(userMessage);
