220ee271b9b01c22baa6ae606ce8a5edf06c619c
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-07-18 13:27:12 -0700

    feat: add exportable apiKey to util.js as default, import it on app.js, add comments with examples of how this would be different if exporting multiple items

---
+++ assets/scripts/app.js
@@ -0,0 +1,4 @@
+// example of import from file that has multiple exportable itmes: import { apiKey } from "./util.js";
+// below is an example of import from file that has a an export of a default (single) item
+import apiKey from "./util.js";
+console.log(apiKey);
---
+++ assets/scripts/util.js
@@ -0,0 +1,3 @@
+// example of export on file where you will be exporting multiple items: export let apiKey = "adfadfadf1";
+// below is an example of export on file where you will be exporting a single item
+export default "adfadfadf1"; 
