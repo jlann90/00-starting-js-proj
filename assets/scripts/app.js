cbf1c72b753224d206625c3e6b057e4cd1c9fb2e
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-07-18 14:40:39 -0700

    feat: add createGreeting function and call it using two constants, greeting1 and greeting2

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,3 +6,16 @@ console.log(apiKey);
 // using 'let' to declare a variable allows for reassignment later, while 'const' is used for variables that should not change
 let userMessage = "Welcome to the JavaScript refresher course!";
 console.log(userMessage);
+
+// Example of creating a function to greet the user, this example has a default for the message parameter
+// If the function is called without a second argument, it will use the default message
+function createGreeting(userName, message = "Hello!"
+) {
+    return "Hi, I am " + userName + ". " + message;
+}
+
+// Create two greeting messages using the createGreeting function
+const greeting1 = createGreeting("James");
+console.log(greeting1);
+const greeting2 = createGreeting("Joe", "Hello, what's up?");
+console.log(greeting2);
